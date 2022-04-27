331a2e7a1d5b1a9383142456e66aeb6792dc568d
Lucas Duport <lucas.duport@epita.fr>
2022-04-27 09:45:16 +0100

    version enemy eliot

--- Assets/Scripts/Player/HitPlayers.cs
+++ Assets/Scripts/Player/HitPlayers.cs
@@ -7,7 +7,7 @@ public class HitPlayers : MonoBehaviour
     public int DamageParCoup = 30;
     void OnTriggerStay2D(Collider2D collider)
     {
-        if (GetComponent<PhotonView>().IsMine && collider.transform.CompareTag("Enemy"))
+        if (collider.transform.CompareTag("Enemy") && collider.GetComponent<PhotonView>().IsMine)
         {
             EnemyHealth eh = collider.transform.GetComponent<EnemyHealth>();
             eh.HealthModifications(-DamageParCoup);
